8bff27561ff6c1c54e83aa4c808efe4f434daca1
Dwiyasa-Nakula <115543100+Dwiyasa-Nakula@users.noreply.github.com>
2024-08-26 22:25:42 +0700

    Add files

--- yolo_video.py
+++ yolo_video.py
@@ -94,8 +94,7 @@ def initializeVideoWriter(video_width, video_height, videoStream):
     return cv2.VideoWriter(outputVideoPath, fourcc, sourceVideofps,
                            (video_width, video_height), True)
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
-# and determine only the *output* layer names that we need from YOLO
+# load our YOLO object detector trained on COCO dataset (80 classes) and determine only the *output* layer names that we need from YOLO
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
@@ -145,9 +144,7 @@ while True:
     if not grabbed:
         break
 
-    # construct a blob from the input frame and then perform a forward
-    # pass of the YOLO object detector, giving us our bounding boxes
-    # and associated probabilities
+    # construct a blob from the input frame and then perform a forward pass of the YOLO object detector, giving us our bounding boxes and associated probabilities
     blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (inputWidth, inputHeight),
                                  swapRB=True, crop=False)
     net.setInput(blob)
@@ -159,25 +156,18 @@ while True:
     for output in layerOutputs:
         # loop over each of the detections
         for i, detection in enumerate(output):
-            # extract the class ID and confidence (i.e., probability)
-            # of the current object detection
+            # extract the class ID and confidence (i.e., probability) of the current object detection
             scores = detection[5:]
             classID = np.argmax(scores)
             confidence = scores[classID]
 
-            # filter out weak predictions by ensuring the detected
-            # probability is greater than the minimum probability
+            # filter out weak predictions by ensuring the detected probability is greater than the minimum probability
             if confidence > preDefinedConfidence:
-                # scale the bounding box coordinates back relative to
-                # the size of the image, keeping in mind that YOLO
-                # actually returns the center (x, y)-coordinates of
-                # the bounding box followed by the boxes' width and
-                # height
+                # scale the bounding box coordinates back relative to the size of the image, keeping in mind that YOLO actually returns the center (x, y)-coordinates of the bounding box followed by the boxes' width and height
                 box = detection[0:4] * np.array([video_width, video_height, video_width, video_height])
                 (centerX, centerY, width, height) = box.astype("int")
 
-                # use the center (x, y)-coordinates to derive the top
-                # and and left corner of the bounding box
+                # use the center (x, y)-coordinates to derive the top and and left corner of the bounding box
                 x = int(centerX - (width / 2))
                 y = int(centerY - (height / 2))
 
